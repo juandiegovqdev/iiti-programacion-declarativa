cfdcde6e9486f801b604bd45854d60b6dbf27647
Juan Diego <juandiegovillalobosquiros@gmail.com>
2021-08-16 14:50:10 +0200

    Update Ejercicio-29-11-2017.hs

--- Ejercicios/Ejercicio-29-11-2017.hs
+++ Ejercicios/Ejercicio-29-11-2017.hs
@@ -2,51 +2,8 @@
 -- Grado de Ingeniería Informática - Tecnologías Informáticas
 -- 2 Parcial                                      29 de Noviembre 2017
 -- -------------------------------------------------------------------
--- Apellidos:
--- Nombre:
--- -------------------------------------------------------------------
--- AVISOS IMPORTANTES
--- · Antes de continuar, cambie el nombre de este archivo por:
---                   <uvus>Ejercicio2.hs
---   donde <uvus> debe ser su usuario virtual.
--- · Escriba la solución de cada ejercicio en el hueco reservado para
---   ello.
--- · Asegúrese de utilizar exactamente el nombre y el tipo indicado
---   para cada función solicitada. Puede añadir tantas funciones
---   auxiliares (incluyendo el tipo adecuadamente) como necesite
---   describiendo claramente su objetivo.
--- -------------------------------------------------------------------
 
 import Test.QuickCheck
-import CodeWorld
-
--- -------------------------------------------------------------------
-
--- -------------------------------------------------------------------
--- Ejercicio 1.
--- -------------------------------------------------------------------
--- (1.1) Definir una función
-
-anillos :: [Double] -> Picture
-anillos ls = undefined
-
--- tal que, dada una lista numérica, devuelva un dibujo de anillo
--- concéntricos siendo los números de la lista los distintos radios.
--- drawingOf (anillos [1,7,5,3]) == Figura (1)
-
--- -------------------------------------------------------------------
--- (1.2) Definir una función
-
-anillosColor :: [Double] -> [Color] -> Picture
-anillosColor = undefined
-
--- tal que, dada una lista numérica (con valores crecientes) y otra de
--- colores (ambas de la misma longitud), devuelva un dibujo de anillos
--- concéntricos cada uno de ellos del color indicado. Utiliza las
--- listas por comprensión para resolver el ejercicio.
-
--- drawingOf (anillosColor [1,3,5,7] [red, green, magenta, azure])
--- Figura (2)
 
 -- -------------------------------------------------------------------
 -- Ejercicio 2.
@@ -54,7 +11,9 @@ anillosColor = undefined
 -- (2.1) Definir una función, utilizando recursión,
 
 alterna :: [a] -> [a] -> [a]
-alterna = undefined
+alterna (x:xs) (y:ys) = x : y : alterna xs ys
+alterna xs [] = xs
+alterna [] ys = ys
 
 -- tal que (alterna xs ys) es la lista obtenida intercalando los
 -- elementos de xs e ys. Por ejemplo,
@@ -67,28 +26,42 @@ alterna = undefined
 -- (2.2) Comprobar con QuickCheck que el número de elementos de
 -- (alterna xs ys) es la suma de los números de elementos de xs e ys.
 
+prop_alterna xs ys = length (alterna xs ys) == (length xs + length ys)
+
 -- -------------------------------------------------------------------
 -- Ejercicio 3. Definir la función
-
-resultadoPositivo :: (Num b, Ord b) => (a -> b) -> [a] -> [a]
-resultadoPositivo = undefined
 
 -- tal que (resultadoPositivo f xs) es la lista de los elementos de la
 -- lista xs tales que el valor de la función f sobre ellos es
 -- positivo. Por ejemplo,
--- resultadoPositivo head [[-1,2],[-9,4],[2,3]]       == [[2,3]]
--- resultadoPositivo sum [[1,2],[9],[-8,3],[],[3,-5]] == [[1,2],[9]]
--- resultadoPositivo (+ 3.5) [1,2,-9,-8.3,-3,-1]      == [1,2,-3,-1]
 -- Definir esta función
--- 1) por recursión (utilizando guardas),
--- 2) por plegado (con 'foldr'),
+
+-- resultadoPositivoR head [[-1,2],[-9,4],[2,3]]       == [[2,3]]
+-- resultadoPositivoR sum [[1,2],[9],[-8,3],[],[3,-5]] == [[1,2],[9]]
+-- resultadoPositivoR (+ 3.5) [1,2,-9,-8.3,-3,-1]      == [1,2,-3,-1]
+-- 1) por recursión (utilizando guardas)
+resultadoPositivoR :: (Num b, Ord b) => (a -> b) -> [a] -> [a]
+resultadoPositivoR f (x:xs)
+    | f x > 0   = x : resultadoPositivoR f xs
+    | otherwise = resultadoPositivoR f xs
+resultadoPositivoR _ [] = []
+
+-- resultadoPositivoP head [[-1,2],[-9,4],[2,3]]       == [[2,3]]
+-- resultadoPositivoP sum [[1,2],[9],[-8,3],[],[3,-5]] == [[1,2],[9]]
+-- resultadoPositivoP (+ 3.5) [1,2,-9,-8.3,-3,-1]      == [1,2,-3,-1]
+-- 2) por plegado (con 'foldr')
+resultadoPositivoP :: (Num b, Ord b) => (a -> b) -> [a] -> [a]
+resultadoPositivoP = undefined
+
+-- resultadoPositivoRec head [[-1,2],[-9,4],[2,3]]       == [[2,3]]
+-- resultadoPositivoRec sum [[1,2],[9],[-8,3],[],[3,-5]] == [[1,2],[9]]
+-- resultadoPositivoRec (+ 3.5) [1,2,-9,-8.3,-3,-1]      == [1,2,-3,-1]
 -- 3) por recursión terminal
+resultadoPositivoRec :: (Num b, Ord b) => (a -> b) -> [a] -> [a]
+resultadoPositivoRec = undefined
 
 -- -------------------------------------------------------------------
 -- Ejercicio 4. Definir la función
-
-alternos :: (a -> b) -> (a -> b) -> [a] -> [b]
-alternos = undefined
 
 -- tal que (alternos f g xs) es la lista obtenida aplicando
 -- alternativamente las funciones f y g a los elementos de la lista
@@ -97,11 +70,13 @@ alternos = undefined
 -- alternos (take 2) reverse ["todo","para","nada"]
 --   == ["to","arap","na"]
 
+alternos :: (a -> b) -> (a -> b) -> [a] -> [b]
+alternos f g (x:y:xs) = f x : g y : alternos f g xs
+alternos f g (x:xs) = f x : alternos f g xs
+alternos f g _ = []
+
 -- -------------------------------------------------------------------
 -- Ejercicio 5. Definir la función
-
-buscaCrucigrama :: Char -> Int -> Int -> [String] -> [String]
-buscaCrucigrama = undefined
 
 -- tal que (buscaCrucigrama x i l ps) es la lista de los elementos de
 -- la lista de palabras ps, que tienen longitud l y tienen la letra x
@@ -109,16 +84,24 @@ buscaCrucigrama = undefined
 -- buscaCrucigrama 'c' 1 7 ["ocaso", "casa", "ocupado"] == ["ocupado"]
 -- Definir esta función utilizando filter y una función anónima.
 
+buscaCrucigrama :: Char -> Int -> Int -> [String] -> [String]
+buscaCrucigrama x i l (p:ps)
+    | length p == l && x == p!!i = p : buscaCrucigrama x i l ps
+    | otherwise = buscaCrucigrama x i l ps
+buscaCrucigrama _ _ _ [] = []
+
 -- -------------------------------------------------------------------
 -- Ejercicio 6. Definir la función
-
-tresDiferentes :: (Eq a) => a -> a -> a -> Bool
-tresDiferentes = undefined
 
 -- tal que (tresDiferentes x y z) se verifica si los elementos x, y y
 -- z son distintos. Por ejemplo,
 --    tresDiferentes 3 5 2  ==  True
 --    tresDiferentes 3 5 3  ==  False
+
+tresDiferentes :: (Eq a) => a -> a -> a -> Bool
+tresDiferentes x y z 
+    | x /= y && x /= z && y /= z = True
+    | otherwise                  = False
 
 -- -------------------------------------------------------------------
 -- Ejercicio 7. Los resultados de las votaciones a delegado en un
@@ -127,25 +110,34 @@ tresDiferentes = undefined
 -- (7.1) Definir el tipo Resultados de tal forma que la siguiente
 -- definición sea correcta.
 
--- votos :: Resultados
--- votos =  [("Ana Perez",10),("Juan Lopez",7), ("Julia Rus", 27),
---           ("Pedro Val",1), ("Pedro Ruiz",27),("Berta Gomez",11)]
+type Resultados = [(String, Int)]
+
+votos :: Resultados
+votos =  [("Ana Perez",10),("Juan Lopez",7), ("Julia Rus", 27), ("Pedro Val",1), ("Pedro Ruiz",27),("Berta Gomez",11)]
  
 -- -------------------------------------------------------------------
 -- (7.2) Define la función
-
--- mayorV :: Resultados -> Int
--- mayorV = undefined
 
 -- tal que (mayorV xs) devuelve el número de votos obtenido por los
 -- ganadores de la votación cuyos resultados se recogen en xs. Por
 -- ejemplo,
 --     mayorV votos == 27
 
+mayorV :: Resultados -> Int
+mayorV xs = mayorVAux xs 0
+
+mayorVAux :: Resultados -> Int -> Int
+mayorVAux ((x,y):xs) a
+    | y > a = mayorVAux xs y
+    | otherwise = mayorVAux xs a
+mayorVAux [] a = a
+
 -- b) Define la función 
 
--- ganadores :: Resultados -> [String]
--- ganadores = undefined
+ganadores :: Resultados -> [String]
+ganadores [] = []
+ganadores ((x,y):xs) = if y == m then x : ganadores xs else ganadores xs
+    where m = mayorV votos
 
 -- tal que (ganadores xs) es la lista de los estudiantes con mayor
 -- número de votos en xs. Por ejemplo,
